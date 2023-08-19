696b3611466af8d428efbd23bf2d8c49f92ceb37
fatenalshim <faten_alshim@yahoo.com>
2023-08-19 15:22:40 -0700

    gix file_printf

--- _alxprintf.c
+++ _alxprintf.c
@@ -24,32 +24,31 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
-			// Flush buffer if it's full
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			// Print the current character
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			// Flush buffer before processing format specifier
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-// Handle format specifier and add printed characters to total
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
-}
-// Flush any remaining content in the buffer
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
+
