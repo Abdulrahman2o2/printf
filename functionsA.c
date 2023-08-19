f37a23331b3390c268e65a9abea7cb10f3e3977c
abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-08-19 16:53:41 -0700

    Added new function.c

---
+++ functionsA.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_unsigned - Prints an unsigned number.
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Active flags
+ * @width: Width specifier
+ * @precision: Precision specifier
+ * @size: Size specifier
+ * Return: Number of characters printed.
+ */
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
