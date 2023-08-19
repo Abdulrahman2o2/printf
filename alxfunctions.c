0a2518dcf19b9715aeb483e3285812a7c1437cf2
fatenalshim <faten_alshim@yahoo.com>
2023-08-19 15:40:37 -0700

    add 3rd  function at alxfunctions

--- alxfunctions.c
+++ alxfunctions.c
@@ -18,6 +18,18 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
+
+/**
+ * print_string - Prints a string
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: Width of the field
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of characters printed
+ */
+
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
