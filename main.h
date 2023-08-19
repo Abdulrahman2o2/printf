8abde35c21b02afeb176db05950ca1f98567eb8e
abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-08-19 16:44:43 -0700

    add utils to main.h

--- main.h
+++ main.h
@@ -88,7 +88,7 @@ int get_size(const char *format, int *i);
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/*Function to print a string in rot 13*/
+/*Function to print a string in ROT13*/
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
@@ -105,3 +105,13 @@ int write_pointer(char buffer[], int ind, int length,
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
+
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+#endif /* MAIN_H */
