37e7165fc7e877b56763dde300d9b98f6a9f45ed
abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-08-21 14:34:46 -0700

    add new funcation utilsA.c

--- utilsA.c
+++ utilsA.c
@@ -35,3 +35,16 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 	return (3);
 }
+/**
+ * check_digit - Checks if a character is a digit
+ * @c: The character to be examined
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
