0a3cb682c74638a8241867e652904458c498e6c4
fatenalshim <faten_alshim@yahoo.com>
2023-08-21 13:01:28 -0700

    add file

--- Alxwrite_handlers.c
+++ Alxwrite_handlers.c
@@ -203,4 +203,62 @@ int write_unsgnd(int is_negative, int ind,
 
 	return (write(1, &buffer[ind], length));
 }
+/**
+ * write_pointer - Handles writing of memory addresses
+ * @buffer: Array of characters to hold the address
+ * @ind: Index where the address starts in the buffer
+ * @length: Length of the address
+ * @width: Width specifier
+ * @flags: Flag to calculate active flags
+ * @padd: Padding character
+ * @extra_c: Extra character for sign or space
+ * @padd_start: Index where padding should start
+ *
+ * This function handles writing memory addresses, considering
+ * alignment, width, padding, and the inclusion of extra characters.
+ *
+ * Return: Number of characters printed
+ */
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start)
+{
+	int i;
+
+	if (width > length)
+	{
+		for (i = 3; i < width - length + 3; i++)
+			buffer[i] = padd;
+		buffer[i] = '\0';
+		if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
+		{
+			buffer[--ind] = 'x';
+			buffer[--ind] = '0';
+			if (extra_c)
+				buffer[--ind] = extra_c;
+			return (write(1, &buffer[ind], length) + write(1, &buffer[3], i - 3));
+		}
+		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buffer */
+		{
+			buffer[--ind] = 'x';
+			buffer[--ind] = '0';
+			if (extra_c)
+				buffer[--ind] = extra_c;
+			return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
+		}
+		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
+		{
+			if (extra_c)
+				buffer[--padd_start] = extra_c;
+			buffer[1] = '0';
+			buffer[2] = 'x';
+			return (write(1, &buffer[padd_start], i - padd_start) +
+				write(1, &buffer[ind], length - (1 - padd_start) - 2));
+		}
+	}
+	buffer[--ind] = 'x';
+	buffer[--ind] = '0';
+	if (extra_c)
+		buffer[--ind] = extra_c;
+	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
+}
 
