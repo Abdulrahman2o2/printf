e815bb0513a4c68821b7266cea176f4d3a3db57c
abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-08-21 14:34:10 -0700

    add new funcation utilsA.c

--- utilsA.c
+++ utilsA.c
@@ -13,3 +13,25 @@ int is_printable(char c)
 
 	return (0);
 }
+/**
+ * add_hexadecimal_code - Appends the ASCII value in hexadecimal format to the buffer
+ * @buffer: Array of characters to store the result.
+ * @i: Index at which to start appending in the buffer.
+ * @ascii_code: The ASCII code to be converted to hexadecimal.
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
