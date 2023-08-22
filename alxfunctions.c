a648c2d1d7e80c7c3ed70a134feb12fe8783633f
fatenalshim <faten_alshim@yahoo.com>
2023-08-22 13:01:31 -0700

    fix betty fil

--- alxfunctions.c
+++ alxfunctions.c
@@ -135,18 +135,16 @@ return (write_number(is_negative, i, buffer, flags, width, precision, size));
  * Return: Number of characters printed
  */
 int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int flags, int width, int precision, int size)
 {
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
-	int count;
-
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-
+unsigned int n, m, i, sum;
+unsigned int a[32];
+int count;
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
 	n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
