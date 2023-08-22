11f19d0b166a1443d1878418db6c5db3c61753f0
fatenalshim <faten_alshim@yahoo.com>
2023-08-22 12:59:13 -0700

    fix betty fil

--- alxfunctions.c
+++ alxfunctions.c
@@ -79,14 +79,14 @@ return (write(1, str, length));
  * Return: Number of characters printed
  */
 int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int flags, int width, int precision, int size)
 {
-	UNUSED(types);
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
+UNUSED(types);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
 	return (write(1, "%%", 1));
 }
 /**
