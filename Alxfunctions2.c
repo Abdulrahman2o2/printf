f8c8fe9950c092d41f49e2bed0ff4d813dd4d9f7
fatenalshim <faten_alshim@yahoo.com>
2023-08-22 12:54:05 -0700

    fix betty fil

--- Alxfunctions2.c
+++ Alxfunctions2.c
@@ -149,16 +149,16 @@ if (in[j] == str[i])
 {
 x = out[j];
 write(1, &x, 1);
-				count++;
-				break;
-			}
-		}
-		if (!in[j])
-		{
-			x = str[i];
-			write(1, &x, 1);
-			count++;
-		}
-	}
-	return (count);
+count++;
+break;
+}
+}
+if (!in[j])
+{
+x = str[i];
+write(1, &x, 1);
+count++;
+}
+}
+return (count);
 }
