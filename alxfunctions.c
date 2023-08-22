45524b4b8c948c2e5c4c4a4a2c4490e018a45204
fatenalshim <faten_alshim@yahoo.com>
2023-08-22 12:54:48 -0700

    fix betty fil

--- alxfunctions.c
+++ alxfunctions.c
@@ -12,9 +12,9 @@
  * Return: Number of characters printed
  */
 int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int flags, int width, int precision, int size)
 {
-	char c = va_arg(types, int);
+char c = va_arg(types, int);
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
