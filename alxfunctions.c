8fcac4312356a21cebc6d4452b6e1d7f7bea51f4
fatenalshim <faten_alshim@yahoo.com>
2023-08-22 13:00:28 -0700

    fix betty fil

--- alxfunctions.c
+++ alxfunctions.c
@@ -108,12 +108,10 @@ long int n = va_arg(types, long int);
 unsigned long int num;
 n = convert_size_number(n, size);
 if (n == 0)
-		buffer[i--] = '0';
-
-	buffer[BUFF_SIZE - 1] = '\0';
-	num = (unsigned long int)n;
-
-	if (n < 0)
+buffer[i--] = '0';
+buffer[BUFF_SIZE - 1] = '\0';
+num = (unsigned long int)n;
+if (n < 0)
 	{
 		num = (unsigned long int)((-1) * n);
 		is_negative = 1;
