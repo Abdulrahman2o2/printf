9d1d4a947abeaf3523b9b47036ef43d9c5224daa
abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-08-22 16:59:54 -0700

    add new description

--- utilsA.c
+++ utilsA.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * is_printable - Determines if a character is printable
+ * is_printable - Determines if a character is printable.
  * @c: The character to be checked.
  *
- * Return: 1 if c is a printable character, 0 otherwise
+ * Return: 1 if c is a printable character, 0 otherwise.
  */
 int is_printable(char c)
 {
